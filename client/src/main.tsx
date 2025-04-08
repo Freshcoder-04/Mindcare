4c9749b8355f7bd339a0d093594be7a9f80b4bf6
Freshcoder-04 <shravanbhai951@gmail.com>
2025-04-08 23:15:21 +0530

    Fixed registration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "./contexts/auth-context";
-import { UserRoleProvider } from "./contexts/user-role-context";
+import { AuthProvider } from "@/contexts/auth-context";
+import { UserRoleProvider } from "@/contexts/user-role-context";
 
 createRoot(document.getElementById("root")!).render(
   <AuthProvider>
